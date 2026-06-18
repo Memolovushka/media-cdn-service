7806c1e416c2bfc485825b45e05a4b6763696923
Memolovushka <gabolov3@gmail.com>
2026-06-18 17:09:19 +0200

    Add Better Auth sentinel client

---
+++ apps/web/src/auth/client.ts
@@ -0,0 +1,8 @@
+"use client";
+
+import { sentinelClient } from "@better-auth/infra/client";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  plugins: [sentinelClient()],
+});
